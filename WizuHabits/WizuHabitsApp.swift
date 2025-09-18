749a63fee8b5404ec7377c96158efb9de83931d4
Damylb18 <160492670+Damylb18@users.noreply.github.com>
2025-09-19 01:32:13 +0300

    Initial Commit

---
+++ WizuHabits/WizuHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizuHabitsApp.swift
+//  WizuHabits
+//
+//  Created by MAC on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WizuHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WizuHabitsTests/WizuHabitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WizuHabitsTests.swift
+//  WizuHabitsTests
+//
+//  Created by MAC on 19/09/2025.
+//
+
+import Testing
+@testable import WizuHabits
+
+struct WizuHabitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
